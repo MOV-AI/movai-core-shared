01c6da910fa0fe5edccae412353ca0cd8325e9de
Dor.M <dor@mov.ai>
2022-04-03 17:14:55 +0300

    another fix

--- movai_core_shared/__init__.py
+++ movai_core_shared/__init__.py
@@ -1,4 +1,13 @@
+"""
+   Copyright (C) Mov.ai  - All Rights Reserved
+   Unauthorized copying of this file, via any medium is strictly prohibited
+   Proprietary and confidential
+
+   Developers:
+   - Dor Marous (Dor@mov.ai) - 2022
+"""
+
 import consts
-import envars
+import envvars
 import exceptions
 import logger
