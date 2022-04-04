8bcf575073fb88acc0f0bfc6b9e4af91d1a6e079
Dor Marcous <engdorm@users.noreply.github.com>
2022-04-04 11:44:40 +0300

    Update __init__.py

--- movai_core_shared/__init__.py
+++ movai_core_shared/__init__.py
@@ -7,7 +7,7 @@
    - Dor Marous (Dor@mov.ai) - 2022
 """
 
-import consts
-import envvars
-import exceptions
-import logger
+import movai_core_shrared.consts
+import movai_core_shrared.envvars
+import movai_core_shrared.exceptions
+import movai_core_shrared.logger
