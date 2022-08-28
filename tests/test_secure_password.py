f3472367c53ceb1b5dec0a186e48cd3685ad9a8a
Erez Zomer <erez@mov.ai>
2022-08-28 20:35:02 +0300

    move movai.core.securepassword to movai_core_shared

---
+++ tests/test_secure_password.py
@@ -0,0 +1,52 @@
+from movai_core_shared.core.securepassword import (SHA256PasswordHash,
+                                                   AESPasswordEncryption)
+import unittest
+from tests.login.raw_test_data import LDAP_PASSWORD
+
+
+class HashPasswordTester(unittest.TestCase):
+
+    def setUp(self) -> None:
+        super().__init__()
+        self.hash = SHA256PasswordHash()
+        self.init_passwords()
+
+    def init_passwords(self) -> None:
+        self.passwords = []
+        for i in range(2):
+            password = f"{LDAP_PASSWORD}{i}"
+            pair = {'pass': password,
+                    'hash': self.hash.create_salted_hash(password)}
+            self.passwords.append(pair)
+
+    def test_verify_password(self) -> int:
+        self.assertTrue(self.hash.verify_password(self.passwords[0]['pass'],
+                                                  self.passwords[0]['hash']))
+        self.assertFalse(self.hash.verify_password(self.passwords[0]['pass'],
+                                                   self.passwords[1]['hash']))
+
+
+class EncryptedPasswords(unittest.TestCase):
+
+    def setUp(self) -> None:
+        super().__init__()
+        self.cipher = AESPasswordEncryption()
+        self.password = LDAP_PASSWORD
+        self.cipher_text = self.cipher.encrypt_password(self.password)
+        self.plain_text = self.cipher.decrypt_password(self.cipher_text)
+
+    def test_encrypt_password(self):
+        self.assertTrue(isinstance(self.cipher_text, bytes),
+                        "cipher text in wrong format")
+        self.assertNotEqual(self.cipher_text, self.password,
+                            "the password is not encrypted")
+
+    def test_decrypt_password(self):
+        self.assertTrue(isinstance(self.plain_text, str),
+                        "Error: plain text is not in str format")
+        self.assertEqual(self.plain_text, self.password,
+                         "plain text and original password do not match")
+
+
+if __name__ == '__main__':
+    unittest.main()
