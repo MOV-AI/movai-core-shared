06e5111b15c84c98544e51cb12742e2fa6e5e7a3
Erez Zomer <erez@mov.ai>
2022-11-17 12:08:29 +0200

    align package version with branch version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setuptools.setup(
     name="movai-core-shared",
-    version="1.0.1-0",
+    version="2.4.0-0",
     author="Backend team",
     author_email="backend@mov.ai",
     description="Shared objects for various Mov.AI projects",
