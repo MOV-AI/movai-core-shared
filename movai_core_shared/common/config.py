51d882c535c20aa718b92131e81cc9dcd5076974
Erez Zomer <erez@mov.ai>
2022-12-28 13:55:36 +0200

    joining all helper functions to utils.py file

--- movai_core_shared/common/config.py
+++
@@ -1,9 +0,0 @@
-from movai_core_shared.envvars import REDIS_MASTER_HOST
-
-def is_manager() -> bool:
-    """Tell the process if its running on the manager host machine.
-
-    Returns:
-        bool: true if the fuction runs on a manager host machine, false otherwise.
-    """
-    return REDIS_MASTER_HOST == "redis-master"
