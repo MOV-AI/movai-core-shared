d66ba00a94ca82db14acadf3842ea32694c48f58
Erez Zomer <erez@mov.ai>
2023-01-10 11:20:38 +0200

    raise version to 2.4.1-0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setuptools.setup(
     name="movai-core-shared",
-    version="2.4.0-1",
+    version="2.4.1-0",
     author="Backend team",
     author_email="backend@mov.ai",
     description="Shared objects for various Mov.AI projects",
