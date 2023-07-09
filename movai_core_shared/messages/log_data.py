0deddbeb4130e2665d8aeaff6a2d341572c060a6
erezz-mov-ai <94361921+erezz-mov-ai@users.noreply.github.com>
2023-07-09 12:15:04 +0300

    BP-984 + 985 - stream logs to frontend (#75)
    * intial ZMQServer class
    
    * adding backend vars for zmq_server
    
    * update zmq_server
    
    * add send_msg func to send a simple msg
    
    * fix error message
    
    * add ZMQServer class
    
    * rearchitecting ZMQServer class
    
    * adding missing envvar
    
    * bind address for log_strreamer
    
    * small fixes for zmq client
    
    * pydantic messages
    
    * asyncio ZMQ server - debug mode not working correctly
    
    * update messages import path
    
    * update send_message func
    
    * add pydantic to setup.py
    
    * fix debug messages
    
    * fix __str__() func
    
    * improvement for ZMQServer
    
    * add helper function
    
    * make ZMQClient asyncio
    
    * update MessageClient to asyncio
    
    * wip: adding beartype to ZMQServer and add lock
    
    * test zmq
    
    * adding beartype to setup.py
    
    * update test
    
    * revert back to sync instead of async
    
    * wip
    
    * call self._close() when exiting the loop
    
    * remove non working tests
    
    * add lock on reading from socket
    
    * remove code duplication with __str__() function
    
    * update the AlertDate to inherit from MetricsData class
    
    * remove empty lines
    
    * black formatting
    
    * sonarr cloud errors
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ movai_core_shared/messages/log_data.py
@@ -0,0 +1,82 @@
+"""
+   Copyright (C) Mov.ai  - All Rights Reserved
+   Unauthorized copying of this file, via any medium is strictly prohibited
+   Proprietary and confidential
+
+   Developers:
+   - Erez Zomer (erez@mov.ai) - 2023
+"""
+from pydantic import BaseModel
+from movai_core_shared.messages.general_data import Request
+
+
+class LogTags(BaseModel):
+    robot: str
+    level: str
+    service: str
+    runtime: bool = False
+
+
+class LogFields(BaseModel):
+    module: str
+    funcName: str
+    lineno: int
+    message: str
+
+
+class LogData(BaseModel):
+    measurement: str
+    log_tags: LogTags
+    log_fields: LogFields
+
+
+class SyslogTags(BaseModel):
+    appname: str
+    facility: str
+    host: str
+    hostname: str
+    severity: str
+
+
+class SyslogFields(BaseModel):
+    module: str
+    funcName: str
+    lineno: int
+    facility_code: int
+    message: str
+    procid: int
+    severity_code: int
+    timestamp: int
+    version: str
+
+
+class SyslogData(BaseModel):
+    measurement: str
+    log_tags: SyslogTags
+    log_fields: SyslogFields
+
+
+class LogRequest(Request):
+    req_data: LogData
+
+    def get_client_log_format(self) -> dict:
+        """Returns a dict with the format used to send to frontend.
+
+        Returns:
+            dict: A dictionary with the required fields.
+        """
+        log_msg = {
+            "time": self.created,
+            "robot": self.robot_info.robot,
+            "service": self.robot_info.service,
+            "module": self.req_data.log_fields.module,
+            "lineno": self.req_data.log_fields.lineno,
+            "funcName": self.req_data.log_fields.funcName,
+            "level": self.req_data.log_tags.level,
+            "message": self.req_data.log_fields.message,
+        }
+        return log_msg
+
+
+class SyslogRequest(Request):
+    req_data: SyslogData
