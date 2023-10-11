42cc5cd3d3aba9503fa2f657ffa3ae8965cacfce
OttoMation-Movai <automation@mov.ai>
2023-10-11 09:19:33 +0000

    [skip actions] Automatic Bump of build version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
 
 setuptools.setup(
     name="movai-core-shared",
-    version="2.4.1-34",
+    version="2.4.1-35",
     author="Backend team",
     author_email="backend@mov.ai",
     description="Shared objects for various Mov.AI projects",
