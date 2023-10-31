9b757a780df082bbc847385af050dae66c9a8364
Erez Zomer <erez@mov.ai>
2023-10-31 15:04:40 +0200

    add source and destination to command data

--- movai_core_shared/messages/command_data.py
+++ movai_core_shared/messages/command_data.py
@@ -1,10 +1,18 @@
 from pydantic import BaseModel
 from movai_core_shared.messages.general_data import Request
 
+class Source(BaseModel):
+    IP: str
+    host: str
+    id: str
+
+class Destination(Source):
+    pass
 
 class Command(BaseModel):
     command: str
     flow: str
+    dst: Destination
 
     def __str__(self):
         return f"Command: {self.command}\n Flow: {self.flow}\n"
