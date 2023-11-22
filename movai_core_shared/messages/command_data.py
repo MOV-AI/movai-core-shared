7b2e4fbee1dc25070b3e5890889a5c7599a81321
Erez Zomer <erez@mov.ai>
2023-11-22 17:27:00 +0200

    remove source

--- movai_core_shared/messages/command_data.py
+++ movai_core_shared/messages/command_data.py
@@ -2,38 +2,38 @@ from typing import Optional
 from pydantic import BaseModel
 from movai_core_shared.messages.general_data import Request
 
-class Source(BaseModel):
+
+class Destination(BaseModel):
     ip: str
     host: str
     id: str
 
-class Destination(Source):
-    pass
 
 class CommandData(BaseModel):
     command: str
     flow: str
     node: Optional[str] = ""
     port: Optional[str] = ""
-    data: Optional[dict] = ""
+    data: Optional[dict] = {}
 
     def __str__(self):
         text = f"Command: {self.command}\n Flow: {self.flow}\n"
         if self.node:
             text += f"Node: {self.node}\n"
-        
+
         if self.port:
             text += f"Port: {self.port}\n"
-        
+
         if self.data:
-            text == f"Data: {self.data}\n"
+            text += f"Data: {self.data}\n"
 
         return text
+
 
 class Command(BaseModel):
     command_data: CommandData
     dst: Destination
 
-    
+
 class CommandReq(Request):
     req_data: Command
