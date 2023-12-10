8d735cad488a3e5847ba24c559f79e2f8c1eaba7
Erez Zomer <erez@mov.ai>
2023-12-10 22:37:16 +0000

    update to List[EmailStr] as field type

--- movai_core_shared/messages/email_data.py
+++ movai_core_shared/messages/email_data.py
@@ -6,17 +6,15 @@
    Developers:
    - Moawiya Mograbi (moawiya@mov.ai) - 2023
 """
-from re import search
 from typing import List, Optional
 
-from pydantic import BaseModel, ConfigDict, field_validator
+from pydantic import BaseModel, ConfigDict, EmailStr, field_validator
 
 from movai_core_shared.envvars import FLEET_NAME, DEVICE_NAME
 from movai_core_shared.consts import NOTIFICATIONS_HANDLER_MSG_TYPE
 from movai_core_shared.logger import Log
 
 
-EMAIL_REGEX = r"([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+"
 logger = Log.get_logger(NOTIFICATIONS_HANDLER_MSG_TYPE)
 
 
@@ -26,7 +24,7 @@ class EmailData(BaseModel):
     validation and check for missing fields or wrong values.
     """
 
-    recipients: List[str]
+    recipients: List[EmailStr]
     notification_type: str = "smtp"
     subject: Optional[str] = None
     body: str
@@ -36,22 +34,19 @@ class EmailData(BaseModel):
     @field_validator("notification_type")
     @classmethod
     def notification_type_valid(cls, value):
+        """Validates the notification_type field
+
+        Args:
+            value (str): Which notification to use.
+
+        Raises:
+            ValueError: In case notification is diffenrent from smtp.
+
+        Returns:
+            str: The given value.
+        """
         if value.lower() != "smtp":
             raise ValueError("notification_type must be smtp")
         return value
-
-    @field_validator("recipients")
-    @classmethod
-    def recipients_valid(cls, value):
-        valid = []
-        for email in value:
-            if not search(EMAIL_REGEX, email):
-                logger.warning(f"email {email} is not a valid email address")
-            else:
-                valid.append(email)
-        if not valid:
-            raise ValueError("no valid Email Address provided for email notification")
-
-        return valid
 
     model_config = ConfigDict(frozen=False)
