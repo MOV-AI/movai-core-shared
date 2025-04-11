785daf32e343383b209d159f668229a8c83be688
duartecoelhomovai <duarte@mov.ai>
2025-04-11 12:36:05 +0100

    revert back the generic adapter

--- movai_core_shared/logger.py
+++ movai_core_shared/logger.py
@@ -60,11 +60,6 @@ LOG_FORMATTER = logging.Formatter(
     LOG_FORMATTER_EXPR,
     datefmt=LOG_FORMATTER_DATETIME,
 )
-CALLBACK_LOG_FORMATTER_EXPR = "[%(levelname)s][%(asctime)s][%(node)s][%(callback)s][%(funcName)s][%(lineno)d]: %(message)s"
-CALLBACK_LOG_FORMATTER = logging.Formatter(
-    CALLBACK_LOG_FORMATTER_EXPR,
-    datefmt=LOG_FORMATTER_DATETIME,
-) 
 
 SEVERETY_CODES_MAPPING = {
     "CRITICAL": syslog.LOG_CRIT,
@@ -82,10 +77,9 @@ class StdOutHandler(logging.StreamHandler):
     _COLORS = SPAWNER_STDOUT_COLORS
     _COLOR_RESET = "\u001b[0m"
 
-    def __init__(self, color=SPAWNER_STDOUT_COLORS, stream=None, formatter_expr=None):
+    def __init__(self, color=SPAWNER_STDOUT_COLORS, stream=None):
         super().__init__(stream)
         self._COLORS = color
-        self.formatter_expr = formatter_expr
 
     def emit(self, record):
         try:
@@ -101,8 +95,6 @@ class StdOutHandler(logging.StreamHandler):
 
             # Add/Remove Tags from log formatter
             _formatter = S_FORMATTER
-            if self.formatter_expr is not None:
-                _formatter = self.formatter_expr
 
             if isinstance(record.args, dict) and record.args.get("tags"):
                 tags = record.args.get("tags")
@@ -227,11 +219,10 @@ def _get_console_handler(stream_config=None):
     Set up the stdout handler
     """
     if stream_config is None:
-        console_handler = StdOutHandler(formatter_expr=LOG_FORMATTER_EXPR)
+        console_handler = StdOutHandler()
         console_handler.setFormatter(LOG_FORMATTER)
     elif stream_config == CALLBACK_LOGGER:
-        console_handler = RuntimeStdOutHandler(stream=sys.stdout)
-        #console_handler.setFormatter(CALLBACK_LOG_FORMATTER)
+        console_handler = CallbackStdOutHandler(stream=sys.stdout)
     else:
         raise ValueError("Unknown stream config for the console logger!")
     #console_handler.setFormatter(LOG_FORMATTER)
@@ -295,8 +286,6 @@ class LogAdapter(logging.LoggerAdapter):
     def __init__(self, logger, **kwargs):
         super().__init__(logger, None)
         self._tags = kwargs
-        self.node = node
-        self.cb = cb
 
     def _exc_tb(self):
         """get latest exception (if any) and format it
@@ -320,9 +309,6 @@ class LogAdapter(logging.LoggerAdapter):
         message += self._exc_tb()
         return message, kwargs
 
-    def info(self, *args, **kwargs):
-        new_msg, kwargs = self.get_message(*args, **kwargs)
-        self.logger.info(new_msg, stacklevel=3, **kwargs)
     def error(self, *args, **kwargs):
         new_msg, kwargs = self.get_message(*args, **kwargs)
         self.logger.error(new_msg, stacklevel=3, **kwargs)
@@ -338,7 +324,7 @@ class LogAdapter(logging.LoggerAdapter):
         raw_tags = dict(kwargs)
         raw_tags.update(self._tags)
         tags = "|".join([f"{k}:{v}" for k, v in raw_tags.items()])
-        kwargs = {"extra": {"tags": raw_tags, "callback": "apanheite", "node": "self.node"}}
+        kwargs = {"extra": {"tags": raw_tags,}}
         
         return f"[{tags}] {msg}", kwargs
 
@@ -386,8 +372,7 @@ class Log:
             LogAdapter: A logger with tags.
         """
         tags[USER_LOG_TAG] = True
-        logging.warning(f"benfiquista {logger_name} {tags}")
-        user_logger = RuntimeLogAdapter(cls.get_logger(logger_name), **tags)
+        user_logger = LogAdapter(cls.get_logger(logger_name), **tags)
 
         return user_logger
 
@@ -403,18 +388,10 @@ class Log:
         Returns:
             LogAdapter: A logger with tags.
         """
-        import inspect
-        frame = inspect.currentframe()
-        outer_frames = inspect.getouterframes(frame)
         tags = {}
-        #tags[USER_LOG_TAG] = True
-        #tags["node"] = node_name
-        #tags["callback"] = callback_name
-        #caller_frame = outer_frames[1]
-        #tags["line"] = caller_frame.lineno
         _logger = cls.get_logger(logger_name, CALLBACK_LOGGER)
         _logger.setLevel(MOVAI_CALLBACK_VERBOSITY_LEVEL)
-        logger = RuntimeLogAdapter(_logger, **tags, node_name=node_name, callback_name=callback_name)
+        logger = CallbackLogAdapter(_logger, **tags, node_name=node_name, callback_name=callback_name)
         return logger
 
 
