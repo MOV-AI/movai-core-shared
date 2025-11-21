3d3d70a58ae69539fa82eba578cc43ef0f4e8bf3
MarcoCunha-MOVAI <marco.cunha@mov.ai>
2025-11-21 18:17:17 +0000

    Removed old Alerts implementation callback

--- movai_core_shared/messages/alert_data.py
+++ movai_core_shared/messages/alert_data.py
@@ -11,8 +11,6 @@ from typing import Union, Literal, Optional
 from pydantic import BaseModel
 
 from movai_core_shared.consts import DeactivationType
-from movai_core_shared.messages.general_data import Request
-from movai_core_shared.messages.metric_data import MetricQueryData
 
 
 class AlertActivationData(BaseModel):
@@ -29,16 +27,3 @@ class AlertDeactivationData(BaseModel):
 
 class AlertData(AlertActivationData, AlertDeactivationData):
     alert_id: str
-
-
-class AlertRequest(Request):
-    req_data: MetricQueryData
-
-
-class Alert(BaseModel):
-    name: str
-    info: str
-    action: str
-    callback: str
-    status: str
-    send_email: bool = False
