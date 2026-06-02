53f895ce956a98fdbc0b50c6e49d1dcd1f8b2a01
David Dias <david.dias@mov.ai>
2026-06-02 14:44:40 +0100

    Remove empty __all__

--- movai_core_shared/messages/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-   Copyright (C) Mov.ai  - All Rights Reserved
-   Unauthorized copying of this file, via any medium is strictly prohibited
-   Proprietary and confidential
-
-   Developers:
-   - Erez Zomer (erez@mov.ai) - 2023
-"""
-__all__ = []  # Empty - import specific modules as needed
