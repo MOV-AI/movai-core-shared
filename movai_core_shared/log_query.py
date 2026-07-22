6ea595585b0cf708f638216ff8ac32dabe2ec089
David Dias <david.dias@mov.ai>
2026-07-22 18:04:42 +0100

    Query Loki draft

--- movai_core_shared/log_query.py
+++ movai_core_shared/log_query.py
@@ -1,19 +1,17 @@
+from typing import List, Optional, Union
+
+import requests
+
 from movai_core_shared.consts import (
     DEFAULT_LOG_LIMIT,
     DEFAULT_LOG_OFFSET,
-    LOGS_QUERY_HANDLER_MSG_TYPE,
-    LOGS_MEASUREMENT,
-)
-from movai_core_shared.envvars import (
-    LOCAL_MESSAGE_SERVER,
-    MASTER_MESSAGE_SERVER,
 )
 from movai_core_shared.messages.metric_data import LogQueryResponse
-from movai_core_shared.core.message_client import AsyncMessageClient
-from movai_core_shared.common.utils import is_manager
 from movai_core_shared.common.time import validate_time
+from movai_core_shared.logger import Log
 from .base_query import BaseQuery
 
+LOGGER = Log.get_logger(__name__)
 
 # pylint: disable=too-many-arguments,too-many-locals,too-many-branches,invalid-name
 
@@ -32,17 +30,12 @@ class LogsQuery(BaseQuery):
         message=None,
         fromDate=None,
         toDate=None,
-        order_by=None,
         order_dir=None,
-        **kwrargs,
+        tags: Optional[Union[List[str], str]] = None,
     ) -> LogQueryResponse:
-        """Get logs from message-server"""
-        server_addr = MASTER_MESSAGE_SERVER
-        if is_manager():
-            server_addr = LOCAL_MESSAGE_SERVER
-
-        message_client = AsyncMessageClient(server_addr)
         params = {}
+        query = {}
+        query_parts = ["logfmt"]
 
         if limit is not None:
             params["limit"] = cls.validate_value("limit", limit)
@@ -51,43 +44,77 @@ class LogsQuery(BaseQuery):
             params["offset"] = cls.validate_value("offset", offset)
 
         if robots is not None:
-            params["robot"] = robots
+            query["robot"] = robots
 
         if level is not None:
-            params["level"] = level
+            query_parts.append(f'detected_level="{level.upper()}"')
 
         if message is not None:
-            params["message"] = cls.validate_message(message)
+            msg = cls.validate_message(message)
+            query_parts.append(f'~"(?i){msg}"')
 
         if fromDate is not None:
-            params["fromDate"] = validate_time(fromDate)
+            params["start"] = int(validate_time(fromDate) * 1e9)
 
         if toDate is not None:
-            params["toDate"] = validate_time(toDate)
+            params["end"] = int(validate_time(toDate) * 1e9)
 
         if services is not None:
-            params["service"] = services
+            query["service_name"] = services
 
-        if order_by is not None:
-            params["order_by"] = order_by
+        if tags is not None:
+            if isinstance(tags, str):
+                tags = [tags]
+            query_parts.append("|".join([f'tags="{tag}:True"' for tag in tags]))
+
+        params["query"] = "{" + ", ".join(f'{k}="{v}"' for k, v in query.items()) + "}"
+
+        if query_parts:
+            params["query"] += "|" + "|".join(query_parts)
+
+        LOGGER.info(f"Querying Loki with params: {params}")
+
+        response = requests.get("http://loki:3100/loki/api/v1/query_range", params=params)
+        response.raise_for_status()
+
+        LOGGER.info(f"Loki response: {response.json()}")
+
+        compatible_data = []
+
+        data = response.json().get("data", {}).get("result", [])
+
+        for msg in data:
+            compatible_data.append(
+                {
+                    "robot": msg["stream"]["robot"],
+                    "level": msg["stream"]["detected_level"],
+                    "service": msg["stream"]["service"],
+                    "runtime": False,
+                    "module": "<string>",
+                    "funcName": "<module>",
+                    "lineno": 1,
+                    "message": msg["stream"]["message"],
+                    "args": None,
+                    "time": int(msg["values"][0][0]) // 1_000_000_000,
+                    "ui": "True",
+                }
+            )
 
         if order_dir is not None:
-            params["order_dir"] = order_dir
+            params["direction"] = "forward"
+        else:
+            compatible_data.reverse()  # reverse when DESC order
 
-        if kwrargs:
-            if "tags" in kwrargs:
-                params["tag"] = kwrargs["tags"]
-            else:
-                params["tag"] = kwrargs
-
-        query_data = {
-            "measurement": LOGS_MEASUREMENT,
-            "query_data": params,
-            "count_field": "message",
+        compatible_response = {
+            "success": True,
+            "results": {
+                "limit": limit,
+                "offset": offset,
+                "count": len(response.json().get("data", {}).get("result", [])),
+                "data": compatible_data,
+            },
         }
 
-        query_response = await message_client.send_request(
-            LOGS_QUERY_HANDLER_MSG_TYPE, query_data, None, True
-        )
+        LOGGER.info(f"Loki compatible response: {compatible_response}")
 
-        return LogQueryResponse(**(query_response["response"]))
+        return LogQueryResponse(**compatible_response)
